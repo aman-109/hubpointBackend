d53633d017bfd871bf9f6a2477815e050dc789a0
aman-109 <aman.mate.52@gmail.com>
2022-12-19 22:13:36 +0530

    fauth and gauth done

--- src/utils/facebookOauth.js
+++ src/utils/facebookOauth.js
@@ -1,6 +1,8 @@
 require("dotenv").config()
 const passport=require("passport")
-const FacebookStrategy=require("passport-facebook")
+const FacebookStrategy=require("passport-facebook");
+const User = require("../model/user.model");
+const { v4: uuidv4 } = require('uuid');
 
 passport.use(new FacebookStrategy({
     clientID: process.env.FACEBOOK_APP_ID,
@@ -8,9 +10,21 @@ passport.use(new FacebookStrategy({
     callbackURL: "https://hubpointserver.onrender.com/user/auth/facebook/callback",
     profileFields: ['id', 'displayName', 'email','picture']
   },
-  function(accessToken, refreshToken, profile, cb) {
-    
-    return cb(null, profile);
+  async function(accessToken, refreshToken, profile, cb) {
+    let email=profile._json.email
+    let name=profile._json.name
+    const user= new User({
+        name,
+        email,
+        password:uuidv4()
+    })
+    await user.save()
+    let payload={
+        email,
+        
+    }
+      return cb(null, payload);
+   
     // console.log(profile._json.picture.data)
   }
 ));
--- src/utils/googleOAuth.js
+++ src/utils/googleOAuth.js
@@ -2,7 +2,8 @@
 require("dotenv").config()
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const passport=require("passport");
-
+const { v4: uuidv4 } = require('uuid');
+const User = require("../model/user.model");
 
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
@@ -13,10 +14,16 @@ passport.use(new GoogleStrategy({
 
     //creating new user
     let email=profile._json.email
+    let name=profile._json.name
+    const user= new User({
+      name,
+        email,
+        password:uuidv4()
+    })
+    await user.save()
     let payload={
         email,
-        url:profile._json.picture
-
+        
     }
       return cb(null, payload);
     // console.log(payload)
