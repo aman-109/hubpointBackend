b733a8a4c64c818b59c56055c42b918420004d08
aman-109 <aman.mate.52@gmail.com>
2022-12-20 10:51:17 +0530

    gauth redirect

--- src/controller/fAuth.js
+++ src/controller/fAuth.js
@@ -1,7 +1,8 @@
 
 
 const facebook=async function(req,res){
-    res.send(req.user)
+    // res.send(req.user)
+    res.redirect("https://hubpoint.vercel.app/marketplace")
 }
 
 module.exports={facebook}
--- src/controller/gAuth.js
+++ src/controller/gAuth.js
@@ -1,7 +1,8 @@
 
 
 const google=async function(req,res){
-    res.send(req.user)
+    // res.send(req.user)
+    res.redirect("https://hubpoint.vercel.app/marketplace")
 }
 
 module.exports={google}
