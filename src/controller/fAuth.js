d40e2832b152dcea943fef11b128a16dcb760067
aman-109 <aman.mate.52@gmail.com>
2022-12-20 13:13:24 +0530

    done

--- src/controller/fAuth.js
+++ src/controller/fAuth.js
@@ -2,7 +2,7 @@
 
 const facebook=async function(req,res){
     // res.send(req.user)
-    res.redirect("https://hubpoint.vercel.app/marketplace")
+    res.redirect("https://hubpoint-aman-109.vercel.app/marketplace")
 }
 
 module.exports={facebook}
